006a21f67cac2aebedb3ed55099a81dc21882537
Henning Leutz <leutz@pcsg.de>
2015-04-18 18:00:26 +0200

    psr-2

--- lib/QUI/YouTube/Controls/Player.php
+++ lib/QUI/YouTube/Controls/Player.php
@@ -16,29 +16,31 @@ class Player extends \QUI\Control
 {
     /**
      * constructor
+     *
      * @param Array $attributes
      */
-    public function __construct($attributes=array())
+    public function __construct($attributes = array())
     {
-        parent::setAttributes( $attributes );
+        parent::setAttributes($attributes);
 
-        $this->setAttribute( 'qui-class', 'package/quiqqer/youtube/bin/Player' );
-        $this->setAttribute( 'class', 'quiqqer-youtube-player' );
+        $this->setAttribute('qui-class', 'package/quiqqer/youtube/bin/Player');
+        $this->setAttribute('class', 'quiqqer-youtube-player');
 
-        $this->setAttribute( 'videos', '' );
-        $this->setAttribute( 'channel', '' );
+        $this->setAttribute('videos', '');
+        $this->setAttribute('channel', '');
     }
 
     /**
      * (non-PHPdoc)
+     *
      * @see \QUI\Control::getBody()
      */
     public function getBody()
     {
-        $str  = '<div ';
-        $str .= 'data-videos="'. $this->getAttribute( 'videos' ) .'" ';
-        $str .= 'data-channel="'. $this->getAttribute( 'channel' ) .'" ';
-        $str .= 'data-style="'. $this->getAttribute( 'style' ) .'" ';
+        $str = '<div ';
+        $str .= 'data-videos="'.$this->getAttribute('videos').'" ';
+        $str .= 'data-channel="'.$this->getAttribute('channel').'" ';
+        $str .= 'data-style="'.$this->getAttribute('style').'" ';
         $str .= '></div>';
 
         return $str;
